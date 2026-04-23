6bec87c91caefa315ca53e00d1a62712f71ad269
Safoora Fatima <fsafura80@gmail.com>
2026-04-23 23:01:38 +0530

    FUNCTION

--- cpp folder.2/practice2.cpp
+++ cpp folder.2/practice2.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
 using namespace std;
-int sum(int num1, int num2) {
-    int num3 = num1 + num2;
-    return num3; //returning the value of num3 to the caller function
-}
+//RETURN FUNCTION
+// int sum(int num1, int num2) {
+//     int num3 = num1 + num2;
+//     return num3; //returning the value of num3 to the caller function
+// }
 
+// int main() {
+//     int num1, num2;
+//     cin >> num1 >> num2;
+//     int result = sum(num1, num2); //storing the returned value of sum function in result variable
+//     cout << result << endl;
+//     return 0;
+// }
+void dosomething(int num) {
+    if(num > 0) {
+        cout << "positive" << endl;
+    }
+    else if(num < 0) {
+        cout << "negative" << endl;
+    }
+    else {
+        cout << "zero" << endl;
+    }
+} 
 int main() {
-    int num1, num2;
-    cin >> num1 >> num2;
-    int result = sum(num1, num2); //storing the returned value of sum function in result variable
-    cout << result << endl;
+    int num;
+    cin >> num;
+    dosomething(num);
     return 0;
 }
