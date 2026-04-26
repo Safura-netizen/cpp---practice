446607923b0767c4b099ba4a0557d9ac31217527
Safoora Fatima <fsafura80@gmail.com>
2026-04-26 22:06:47 +0530

    empty

--- cpp folder.2/practice3.cpp
+++ cpp folder.2/practice3.cpp
@@ -27,13 +27,14 @@ using namespace std;
 // return 0;
 // }
 //PASS BY REFERENCE (it takes original value and changes it, no copies made ORIGINAL value itself is changed)
-void doSomething(string &s){  //for string pass by reference we use & sign after the data type and before the variable name 
-    s[0] = 't';
-    cout<<s<<endl;
-}
-int main(){
-    string s = "raj";
-    doSomething(s);
-    cout<<s<<endl;
-return 0;
-}
+// void doSomething(string &s){  //for string pass by reference we use & sign after the data type and before the variable name 
+//     s[0] = 't';
+//     cout<<s<<endl;
+// }
+// int main(){
+//     string s = "raj";
+//     doSomething(s);
+//     cout<<s<<endl;
+// return 0;
+// }
+//array and string are passed by reference by default in c++ so we can change the original value without using & sign but for other data types we need to use & sign to pass by reference
